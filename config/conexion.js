e25ef27676152ce78ceb7d82dcd7522f8deac009
JeissonRojas20 <159955413+JeissonRojas20@users.noreply.github.com>
2024-11-25 23:04:38 -0500

    Update conexion.js

--- config/conexion.js
+++ config/conexion.js
@@ -1,12 +1,9 @@
 const mysql = require("mysql");
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT} = require('../config.js')
-
 const db = mysql.createConnection({
-    host:DB_HOST,
-    user:DB_USER,
-    password:DB_PASSWORD,
-    database:DB_NAME,
-    port: DB_PORT
+    host: "mysql.railway.internal",
+    user: "root",
+    password: "cWgMMDSxXKsbafEqTyMuGvqpopLrVTro",
+    database: "railway"
 });
 
 db.connect(err => {
