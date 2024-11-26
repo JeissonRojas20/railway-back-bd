56718484c437dcf2e56aad7b2550b40227488fae
JeissonRojas20 <159955413+JeissonRojas20@users.noreply.github.com>
2024-11-25 22:42:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
-export const PORT = process.env.PORT || 3000
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_HOST || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-export const DB_NAME = process.env.DB_NAME || 'proyectoo'
-export const DB_PORT = process.env.DB_PORT || '3307'
+// config.js
+
+const PORT = process.env.PORT || 3000;
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
+const DB_NAME = process.env.DB_NAME || 'proyectoo';
+const DB_PORT = process.env.DB_PORT || '3307';
+
+module.exports = {
+    PORT,
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT
+};
