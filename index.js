a66c22d4ae7a65449fb560e5901655f277f68ef8
JeissonRojas20 <159955413+JeissonRojas20@users.noreply.github.com>
2024-11-27 19:49:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const path = require('path'); 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://vercel-front-gi7ode53m-jeissonrojas20s-projects.vercel.app/',
     credentials: true
 }));
 app.use(express.json());
